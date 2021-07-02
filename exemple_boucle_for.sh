19779cd2aaac46a151f9ae13b376980ff7dfdefe
Pierre <psable@dawan.fr>
2021-07-02 10:48:58 +0200

    internal field separator

--- exemple_boucle_for.sh
+++ exemple_boucle_for.sh
@@ -9,3 +9,10 @@ for rep_home in $(cat /etc/passwd | cut -d ':' -f 6)
 do
   echo "Contenu de $rep_home" && ls -l $rep_home
 done
+
+# Changement variable IFS pour definir un separateur de champ precis
+var="toto:titi:tutu"
+
+IFS=:
+
+for i in $var; do echo $i; done
